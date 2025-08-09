065bd86a3439695145a16c5d964bf9f7f7235875
vyshnavi-0210 <balinenivyshnavi@gmail.com>
2025-08-09 15:12:57 +0530

    Add files via upload

---
+++ lab4/student/Test.java
@@ -0,0 +1,17 @@
+package student;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		rollnumber r=new rollnumber();
+		r.rno=56;
+		r.grade="B";
+		r.name="Sanju";
+		System.out.println(r.name);
+		System.out.println(r.grade);
+		
+
+	}
+
+}
---
+++ lab4/student/rollnumber.java
@@ -0,0 +1,9 @@
+package student;
+
+public class rollnumber {
+
+	private int rno;
+	protected String grade;
+	public String name;
+
+}
